c78861154aa747917f8991e1576abd96cfaa220e
Noah Smith <noah.smith@metzohanian.com>
2022-08-13 13:02:32 +0000

    Implement basic smui material ui theme

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Button, { Label } from '@smui/button';
+	import Paper, { Title, Subtitle, Content } from '@smui/paper';
+
+	let clicked = 0;
+</script>
+
+<Paper>
+	<Title>Welcome to SvelteKit</Title>
+	<Content>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+		<Button on:click={() => clicked++}>
+			<Label>Default</Label>
+		</Button>
+
+		{clicked}
+	</Content>
+</Paper>
